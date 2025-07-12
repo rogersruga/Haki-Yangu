56ba57775b45ace38005fa0dfd55fe2dda67a09d
rogersruga <rogersruga.g@gmail.com>
2025-07-12 21:29:37 +0300

    added ndkVersion = "27.0.12077973"

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,6 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
-        ...
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
